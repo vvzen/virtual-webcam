e242781851d6f09b7021d4fa46d44392371a2570
Valerio Viperino <valerio.viperino@framestore.com>
2022-07-15 22:56:21 +0100

    Square checkpoint

--- js/threejs-renderer.js
+++ js/threejs-renderer.js
@@ -103,40 +103,41 @@ class ThreeJSRenderer {
 
         // Hardcoded for now, but this should come after reading the video.videoWidth and video.videoHeight
         // Sadly, their value can only be accessed at a specific time (using a callback)
-        const webCamWidth = 640;
-        const webCamHeight = 480;
+        //const webCamWidth = 640;
+        //const webCamHeight = 480;
 
-        const gridWidth = webCamWidth * (1 / this.subDivisionLevel);
-        const gridHeight = webCamHeight * (1 / this.subDivisionLevel);
+        //const gridWidth = webCamWidth * (1 / this.subDivisionLevel);
+        //const gridHeight = webCamHeight * (1 / this.subDivisionLevel);
 
-        this.starting = new Vector2(
-            - (gridWidth  * 0.5),
-            - (gridHeight * 0.5)
-        );
-        this.ending = new Vector2(
-            + (gridWidth  * 0.5),
-            + (gridHeight * 0.5)
-        );
+        //this.starting = new Vector2(
+        //    - (gridWidth  * 0.5),
+        //    - (gridHeight * 0.5)
+        //);
+        //this.ending = new Vector2(
+        //    + (gridWidth  * 0.5),
+        //    + (gridHeight * 0.5)
+        //);
 
-        this.numColumns = 640;
+        this.numColumns = 480;
         this.numRows =    480;
         this.totalPoints = this.numRows * this.numColumns;
 
-        console.log(`Start: ${this.starting.x}, ${this.starting.y}. End: ${this.ending.x}, ${this.ending.y}`);
+        //console.log(`Start: ${this.starting.x}, ${this.starting.y}. End: ${this.ending.x}, ${this.ending.y}`);
         console.log(`We created ${this.numColumns} columns and ${this.numRows} rows`);
-        console.log(`We will create ${this.totalPoints * 4} buffered attributes and ${this.totalPoints} points`);
+        console.log(`We will create ${this.totalPoints * 3} buffered attributes and ${this.totalPoints} points`);
 
-        const positions = new Float32Array(this.totalPoints * 4);
+        const positions = new Float32Array(this.totalPoints * 3);
         //let colors = new Float32Array(this.totalPoints * 3);
-        let colors = new Float32Array(this.totalPoints * 4);
+        let colors = new Float32Array(this.totalPoints * 3);
 
-        let index = 0;
         console.log("Creating points..");
+        let index = 0;
 
         //for (let x = this.starting.x; x <= this.ending.x; x+=this.spacing){
         //    for (let y = this.starting.y; y <= this.ending.y; y+=this.spacing){
-        for (let y = this.numRows; y > 0; y--){
+        for (let y = 0; y >= -this.numRows; y--){
             for (let x = 0; x <= this.numColumns; x++){
+            //for (let x = 0; x <= this.numColumns; x++){
                 //console.log(index);
 
                 // Position
@@ -144,17 +145,17 @@ class ThreeJSRenderer {
                 positions[index + 1] = y;
                 positions[index + 2] = 100;
 
-                index += 4;
+                index += 3;
             }
         }
 
         //console.log(`We created ${this.totalPoints} points out of a max of ${this.MAX_POINTS_NUM}`);
 
         const geometry = new BufferGeometry();
-        geometry.setAttribute('position', new Float32BufferAttribute(positions, 4));
-        geometry.setAttribute('color', new Float32BufferAttribute(colors, 4));
+        geometry.setAttribute('position', new Float32BufferAttribute(positions, 3));
+        geometry.setAttribute('color', new Float32BufferAttribute(colors, 3));
 
-        const pointsSize = 2;
+        const pointsSize = 1;
         const material = new PointsMaterial({
             size: pointsSize,
             vertexColors: true
@@ -172,45 +173,56 @@ class ThreeJSRenderer {
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         //document.body.appendChild(this.renderer.domElement);
 
-        console.log("this.subDivisionLevel: ", this.subDivisionLevel);
-        let indexForCam = remapIndex(0, 0, this.numColumns, this.subDivisionLevel);
-        console.log("indexForCam", indexForCam);
+        //console.log("this.subDivisionLevel: ", this.subDivisionLevel);
+        //let indexForCam = remapIndex(0, 0, this.numColumns, this.subDivisionLevel);
+        //console.log("indexForCam", indexForCam);
 
         this.invisibleCanvas = document.createElement('canvas');
-        this.invisibleCanvas.width = video.videoWidth;
+        this.invisibleCanvas.width = video.videoHeight;
         this.invisibleCanvas.height = video.videoHeight;
         this.ctx = this.invisibleCanvas.getContext('2d');
-        this.ctx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
-        const allVideoColors = this.ctx.getImageData(0, 0, video.videoWidth, video.videoHeight).data;
-        console.log(allVideoColors);
-        console.log(
-             `First four colors of video: ${allVideoColors[0]}, ${allVideoColors[1]}, ${allVideoColors[2]}, ${allVideoColors[3]}`
-        );
-        console.log(this.ctx);
+        //this.ctx.drawImage(this.video, 0, 0, this.video.videoWidth, this.video.videoHeight);
+        //const allVideoColors = this.ctx.getImageData(0, 0, video.videoWidth, video.videoHeight).data;
+        //console.log(allVideoColors);
+        //console.log(
+        //     `First four colors of video: ${allVideoColors[0]}, ${allVideoColors[1]}, ${allVideoColors[2]}, ${allVideoColors[3]}`
+        //);
+        //console.log(this.ctx);
+        //console.log(`Number of elements in camera: ${allVideoColors.length}`);
+        //console.log(`Number of points: ${colors.length}`);
     }
 
     render() {
 
         // TODO: Check if OffscreenCanvas can help here
         // Draw the video first on another canvas, then read its colors by accessing that canvas
-        this.ctx.drawImage(this.video, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
-        console.log(`videoWidth: ${this.video.videoWidth}, videoHeight: ${this.video.videoHeight}`);
+        this.ctx.drawImage(this.video, 0, 0, this.ctx.canvas.height, this.ctx.canvas.height);
+        //console.log(`videoWidth: ${this.video.videoWidth}, videoHeight: ${this.video.videoHeight}`);
 
         // This returns a Uint8ClampedArray
-        const videoData = this.ctx.getImageData(0, 0, this.ctx.canvas.width, this.ctx.canvas.height).data;
+        const videoData = this.ctx.getImageData(0, 0, this.ctx.canvas.height, this.ctx.canvas.height).data;
 
         let colors = this.points.geometry.attributes.color.array;
 
-        for (let i = videoData.length; i > 0; i-=4){
-        //for (let i = 0; i < videoData.length; i+=4){
+        let remappedColors = [];
+        for (let index = 0; index < videoData.length; index+=4){
+            const r = videoData[index + 0];
+            const g = videoData[index + 1];
+            const b = videoData[index + 2];
+            remappedColors.push(r, g, b);
+        }
+
+        //for (let i = videoData.length; i > 0; i-=4){
+        //for (let i = 0; i < remappedColors.length; i+=3){
+        for (let i = 0; i < remappedColors.length; i+=3){
 
             //let point = xyFromIndex(i, this.numColumns);
             //let newIndex = remapIndex(point.x, point.y, this.numColumns, this.subDivisionLevel);
 
-            colors[i + 0] = videoData[i + 0] / 255;
-            colors[i + 1] = videoData[i + 1] / 255;
-            colors[i + 2] = videoData[i + 2] / 255;
-            colors[i + 3] = videoData[i + 3] / 255;
+            colors[i + 0] = remappedColors[i + 0] / 255;
+            colors[i + 1] = remappedColors[i + 1] / 255;
+            colors[i + 2] = remappedColors[i + 2] / 255;
+            //colors[i + 3] = videoData[i + 3] / 255;
         }
 
         this.points.geometry.attributes.position.needsUpdate = true; // required after the first render
